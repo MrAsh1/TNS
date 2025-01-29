6883125fc6e4c26ebef1c63ff9f406e410d43fc7
Ashish <chavanashish4147@gmail.com>
2025-01-29 14:23:28 +0530

    Day1

---
+++ src/Day2/identifierDemo.java
@@ -0,0 +1,9 @@
+package Day2;
+
+public class identifierDemo {
+    public static void main (String[]args){
+        int $num =23;
+        int num =12;
+        String studentName = "Ashish Chavan";
+    }
+}
