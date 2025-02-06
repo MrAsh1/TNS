5f4575f0a517be997ffa60f688ed397fec83d31a
Ashish <chavanashish4147@gmail.com>
2025-02-06 13:39:33 +0530

    Day3

--- src/Day2/identifierDemo.java
+++ src/Day2/identifierDemo.java
@@ -5,5 +5,18 @@ public class identifierDemo {
         int $num =23;
         int num =12;
         String studentName = "Ashish Chavan";
+        //byte
+        byte bytemax = 127;
+        byte bytemin = -128;
+        //short ---2byte
+        short shortmin = 32767;
+        short shortmax = -32679;
+        // int ---4byte
+        int intMin = 214703647;
+        int intMax = - 214703648;
+        /*
+        - Primitive data types have predefined size
+        - Non - primitive data types have dynamic size
+         */
     }
 }
---
+++ src/Day3/increment.java
@@ -0,0 +1,10 @@
+package Day3;
+
+public class increment {
+    public static void main(String[] args) {
+    int a = 10;
+    int b = 10;
+    System.out.println(++a);
+    System.out.println(b++);
+    }
+}
